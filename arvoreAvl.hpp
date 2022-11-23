5451faf2584051ad20eb3119f87588cf1ddf9b3d
Diego Camargo <diego.camargo@injunior.com.br>
2022-11-23 16:30:47 -0300

    Implementação das funções auxiliares maior e calcAltura e uma função para calcular o fator de balanceamento

--- arvoreAvl.hpp
+++ arvoreAvl.hpp
@@ -15,4 +15,25 @@
     this->raiz = novoNo;
   };
 
+  int maior (int a, int b) {
+    if(a>b)
+      return a;
+    else
+      return b;
+  };
+
+  int calcAltura (No* no) {
+    if(no)
+      return no->altura;
+    else
+      return 0;
+  };
+
+  int calcFatorBalanceamento (No* no) {
+    if(no)
+      return (calcAltura(no->esq) - calcAltura(no->dir));
+    else
+      return 0;
+  };
+
 };
