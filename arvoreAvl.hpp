c56c3bfead11f85008961d66bf06a13a2d18d36d
Diego Camargo <diego.camargo@injunior.com.br>
2022-11-23 16:31:20 -0300

    Implementação das funções de rotação da arvore

--- arvoreAvl.hpp
+++ arvoreAvl.hpp
@@ -36,4 +36,50 @@
       return 0;
   };
 
+  No* rotEsq(No *raiz){
+    No *subDir, *filhoEsq;
+  
+    subDir = raiz->dir;
+    filhoEsq = subDir->esq;
+  
+    subDir->esq = raiz;
+    raiz->dir = filhoEsq;
+  
+    raiz->altura = maior(calcAltura(raiz->esq), calcAltura(raiz->dir)) + 1;
+    subDir->altura = maior(calcAltura(subDir->esq), calcAltura(subDir->dir)) + 1;
+  
+    if(this->raiz == raiz)
+      this->raiz = subDir;
+  
+    return subDir;
+  }
+
+  No* rotDir(No *raiz){
+      No *subEsq, *filhoDir;
+  
+      subEsq = raiz->esq;
+      filhoDir = subEsq->dir;
+  
+      subEsq->dir = raiz;
+      raiz->esq = filhoDir;
+  
+      raiz->altura = maior(calcAltura(raiz->esq), calcAltura(raiz->dir)) + 1;
+      subEsq->altura = maior(calcAltura(subEsq->esq), calcAltura(subEsq->dir)) + 1;
+    
+      if(this->raiz == raiz)
+        this->raiz = subEsq;
+  
+      return subEsq;
+  }
+
+  No* rotEsqDir(No *raiz){
+      raiz->esq = rotEsq(raiz->esq);
+      return rotDir(raiz);
+  }
+
+  No* rotDirEsq(No *raiz){
+      raiz->dir = rotDir(raiz->dir);
+      return rotEsq(raiz);
+  }
+
 };
